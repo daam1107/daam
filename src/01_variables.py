11cf3bd8bf2915c795e2bb41220ba25269ad85c0
Diego Apablaza <daam@MacBook-Pro-de-Diego.local>
2021-07-01 18:18:58 -0400

    se cargan los archivos iniciales del curso

---
+++ src/01_variables.py
@@ -0,0 +1,27 @@
+"""
+primitivos: enteros, flotantes, string, boolean
+"""
+print('hello world')
+
+entero = 10
+print(entero)
+print(type(entero))
+
+flotante = 0.8
+print(flotante)
+print(type(flotante))
+
+nombre = 'Diego'
+print(nombre)
+print(type(nombre))
+
+flag = True
+print(flag)
+
+# Operaciones basicas en python
+
+numero1 = 10 
+numero2 = 20
+suma = numero1 + numero2
+
+print(f'la suma es: {suma}')
